0ff8c81d3e365f7a29a6ec2a10205929e5a52635
net pra <xyz@domain.com>
2024-03-01 14:19:01 +0000

    added defensive programming

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ def calculation(x,y,operator):
 num1 = float(input("Please enter number: "))
 num2 = float(input("Please enter number: "))
 
+ops = ['+', '-', '*', '/']
+while True:
 operator = input("Enter the math symbol you would like to use")
-
+if operator not in ops:
+  print("Please selects math sysmbol only from '+, -, *, /'")
+  continue
+else:
+  print("Calculating")
+  break
 calc = calculation(num1, num2, operator)
 
 print(calc)
